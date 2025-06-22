20f3d6f055308fb8cdaccf96d5868d69194bc3d5
EddieSmith <145656018+lordwee@users.noreply.github.com>
2025-06-22 20:15:01 +0200

    Added buttons and layed out the html elements same basic css styling for the container and flex elements

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # calculator
+
+Yeah, let's goooooooo!!!
---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return a + b;
+ }
+function subtract(a, b) {
+    return a - b;
+ }
+function multiply(a, b) {
+    return a * b;
+ }
+function divide(a, b) {
+    
+    let result = a / b;
+    return result.toFixed(2);
+}
+ 
+function operate() {
+    
+}
+
+console.log(add(14, 24));
+console.log(subtract(14, 24));
+console.log(multiply(14, 24));
+console.log(divide(14, 24));
