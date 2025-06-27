cabf45f3f7df436b84a7a2054767879ac6dc2681
EddieSmith <145656018+lordwee@users.noreply.github.com>
2025-06-27 19:33:07 +0200

    I finally finished it. It meets all the criteria which were listed on the site and 2 extra credit. Next step: working on keyboard support...

--- script.js
+++ script.js
@@ -1,17 +1,114 @@
 
 let screen = document.querySelector('.screen');
 const calculator = document.querySelector(".container");
+const add = document.querySelector(".add");
+const subtract = document.querySelector(".subtract");
+const multiply = document.querySelector(".multiply");
+const divide = document.querySelector(".divide");
+
 let input = [];
-let numberOne;
-let numberTwo;
+let numberOne = null;
+let numberTwo = null;
 let result;
 let operator;
+let operatorIsClicked = false;
 
+   
+// FUNCTIONS ____________________________________________________
+function doAdd(a, b) {
+    return a + b;
+}
+function doSubtract(a, b) {
+    return a - b;
+}
+function doDivide (a, b) {
+    return a / b;
+}
+function doMultiply (a, b) {
+        return a * b;
+}
+function getInput(item) {
+    input.push(item)
+    screen.textContent = input.join('');
+}
+function clear() {
+    input = [];
+    screen.textContent = null;
+    operator = undefined;
+    numberOne = null;
+    numberTwo = null;
+    operatorIsClicked = false;
+    result = 0;   
+}
+
+function selectOperator(event) {
+    let target = event.target;
+    let useFunction;
+    switch (target.className) {
+        case 'add':
+            useFunction = doAdd;
+            break;
+        case 'subtract':
+            useFunction = doSubtract;
+            break;
+        case 'multiply':
+            useFunction = doMultiply;
+            break;
+        case 'divide':
+            useFunction = doDivide;
+            break;
+    }
+    if (input.length === 0) {
+        event.preventDefault();
+    } else {
+        if (operatorIsClicked && input.length !== 0) {
+            numberTwo = parseFloat(input.join(''));
+            operate(numberOne, numberTwo, operator);
+            operatorIsClicked = true;
+            operator = useFunction;
+            screen.textContent = result;
+            numberOne = result;
+
+        } else if (operatorIsClicked && input.length === 0) {
+            event.preventDefault();
+            operator = useFunction;
+        } else {
+            operatorIsClicked = true;
+            operator = useFunction
+            numberOne = parseFloat(input.join(''));
+            input = [];
+        }
+    }
+}
+
+function operate(inputOne, inputTwo, func) {
+    if (numberTwo === 0 && operator === doDivide) {
+        alert("Damn you!! How dare you divide a number with zero?!?! Nice try =)");
+        clear();
+    } else {
+        result = func(inputOne, inputTwo);
+        input = [];
+        operatorIsClicked = false;
+    } 
+    return result
+}
+// END OF FUNCTIONS
+
+// EVENTLISTENERS
+window.addEventListener("keydown", event => {
+    console.log(event.code);
+})
 calculator.addEventListener('click', (event) => {
     let target = event.target;
     switch (target.className) {
         case 'point':
-            getInput(".");
+            if (input.length === 0) {
+                event.preventDefault();
+            } else if (input.includes(".")) {
+                event.preventDefault();
+            } else {
+                getInput(".");
+            }
             break;
         case 'zero':
             getInput(0);
@@ -42,65 +139,37 @@ calculator.addEventListener('click', (event) => {
             break;
         case 'nine':
             getInput(9);
-            
             break;
-        case 'add':
-            numberOne = numberTwo;
-            operator = doAdd;
-            input = [];
-            break;
-        case 'subtract':
-            numberOne = numberTwo;
-            input = [];
-            operator = doSubtract;
-            break;
-        case 'multiply':
-            numberOne = numberTwo;
-            operator = doMultiply;
-            input = [];
-            break;
-        case 'divide':
-            numberOne = numberTwo;
-            operator = doDivide;
-            input = [];
-            break;
+        
         case 'ac':
-            input = [];
-            screen.textContent = '0';
+            clear();
             break;
         case 'del':
-            console.log("del");
+            input.pop();
+            screen.textContent = input.join('');
+
             break;
         case 'equals':
-            operate(numberOne, numberTwo, operator);
-            screen.textContent = result;
-            break;
+            if (numberOne !== null && input.length !== 0) {
+                numberTwo = parseFloat(input.join(''));
+                operate(numberOne, numberTwo, operator);
+                if (result !== Math.floor(result)) {
+                    screen.textContent = result.toFixed(2);
+                } else {
+                    screen.textContent = result;
+                }
+                operator = undefined;
+                numberOne = null;
+                numberTwo = null;
+                result = null;
+                break;
+            }
+            
     }
 });
 
-   
-// FUNCTIONS ____________________________________________________
-
- function getInput(item) {
-     input.push(item)
-     screen.textContent = input.join('');
-     return numberTwo = (parseFloat(input.join(''))); 
-}  
-
-function doAdd(a, b) {
-    return a + b;
-}
-function doSubtract(a, b) {
-    return a - b;
-}
-function doDivide (a, b) {
-    return a / b
-}
-function doMultiply (a, b) {
-        return a * b;
-}
-function operate(inputOne, inputTwo, func) {
-    result = func(inputOne, inputTwo);
-    return result;
-}
+add.addEventListener("click", selectOperator);
+subtract.addEventListener("click", selectOperator);
+multiply.addEventListener("click", selectOperator);
+divide.addEventListener("click", selectOperator);
 
