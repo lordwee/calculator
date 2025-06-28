73a09bf1568102d16f23c181c5fd5b4169e2ec10
EddieSmith <145656018+lordwee@users.noreply.github.com>
2025-06-28 20:57:31 +0200

    Finally I managed to add keyboard support and it works greatly, haha

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const subtract = document.querySelector(".subtract");
 const multiply = document.querySelector(".multiply");
 const divide = document.querySelector(".divide");
 
+
+
 let input = [];
 let numberOne = null;
 let numberTwo = null;
@@ -42,8 +44,10 @@ function clear() {
 }
 
 function selectOperator(event) {
+
     let target = event.target;
     let useFunction;
+
     switch (target.className) {
         case 'add':
             useFunction = doAdd;
@@ -95,11 +99,81 @@ function operate(inputOne, inputTwo, func) {
 // END OF FUNCTIONS
 
 // EVENTLISTENERS
-window.addEventListener("keydown", event => {
-    console.log(event.code);
+window.addEventListener("keydown", (event) => {
+    console.log();
+    switch (event.key) {
+        case '.':
+            if (input.length === 0) {
+                event.preventDefault();
+            } else if (input.includes(".")) {
+                event.preventDefault();
+            } else {
+                getInput(".");
+            }
+            break;
+        case "0":
+            getInput(0);
+            break;
+        case '1':
+            getInput(1);
+            break;
+        case '2':
+            getInput(2);
+            break;
+        case '3':
+            getInput(3);
+            break;
+        case '4':
+            getInput(4);
+            break;
+        case '5':
+            getInput(5);
+            break;
+        case '6':
+            getInput(6);
+            break;
+        case '7':
+            getInput(7);
+            break;
+        case '8':
+            getInput(8);
+            break;
+        case '9':
+            getInput(9);
+            break;
+        
+        case 'c':
+            console.log("calculator is emptied");
+            clear();
+            break;
+        case 'Backspace':
+            input.pop();
+            screen.textContent = input.join('');
+
+            break;
+        case "Enter":
+            document.querySelector(".equals").focus();
+                if (numberOne !== null && input.length !== 0) {
+                    numberTwo = parseFloat(input.join(''));
+                    operate(numberOne, numberTwo, operator);
+                    if (result !== Math.floor(result)) {
+                        screen.textContent = result.toFixed(2);
+                    } else {
+                        screen.textContent = result;
+                    }
+                    operator = undefined;
+                    numberOne = null;
+                    numberTwo = null;
+                    result = null;
+                
+                }
+                break;
+            }
+       
 })
 calculator.addEventListener('click', (event) => {
     let target = event.target;
+    
     switch (target.className) {
         case 'point':
             if (input.length === 0) {
@@ -150,6 +224,7 @@ calculator.addEventListener('click', (event) => {
 
             break;
         case 'equals':
+            
             if (numberOne !== null && input.length !== 0) {
                 numberTwo = parseFloat(input.join(''));
                 operate(numberOne, numberTwo, operator);
@@ -167,9 +242,21 @@ calculator.addEventListener('click', (event) => {
             
     }
 });
-
 add.addEventListener("click", selectOperator);
 subtract.addEventListener("click", selectOperator);
 multiply.addEventListener("click", selectOperator);
 divide.addEventListener("click", selectOperator);
+
+window.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "+": add.click();
+            break;
+        case "-": subtract.click();
+            break;
+        case "*": multiply.click();
+            break;
+        case "/": divide.click();
+            break;
+    }
+});
 
